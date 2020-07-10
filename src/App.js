e3d3a234bff0c04161aaeaa1fb3e3c08f477df7b
aitsamahad <aitsama.ahad@gmail.com>
2020-07-10 14:29:16 +0500

    Bootcamp assignment 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import { Cards, CountryPicker, Chart } from './components';
-import { fetchData } from './api/';
-import styles from './App.module.css';
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api/";
+import styles from "./App.module.css";
 
-import image from './images/image.png';
+import image from "./images/image.png";
+import Wave from "./components/Wave";
 
 class App extends React.Component {
   state = {
     data: {},
-    country: '',
-  }
+    country: "",
+  };
 
   async componentDidMount() {
     const data = await fetchData();
@@ -18,21 +19,29 @@ class App extends React.Component {
     this.setState({ data });
   }
 
-  handleCountryChange = async (country) => {
+  handleCountryChange = async country => {
     const data = await fetchData(country);
 
     this.setState({ data, country: country });
-  }
+  };
 
   render() {
     const { data, country } = this.state;
 
     return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} /> 
+      <div>
+        <div className={styles.container}>
+          <div className="hero">
+            <div className="heroGroup">
+              <Cards data={data} />
+              <Wave />
+            </div>
+          </div>
+          <img className={styles.image} src={image} alt="COVID-19" />
+          {/* <Cards data={data} /> */}
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} />
+        </div>
       </div>
     );
   }
